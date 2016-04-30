af228353df5e7470248e7ad6555e30e657894b6a
unknown <volodymyr.rogulya@gmail.com>
2016-04-30 15:53:17 +0300

    change id type to long

--- src/main/java/ua/softserveinc/tc/entity/City.java
+++ src/main/java/ua/softserveinc/tc/entity/City.java
@@ -16,7 +16,7 @@ public class City {
     @GenericGenerator(name = "generator", strategy = "increment")
     @GeneratedValue(generator = "generator")
     @Column(name = "city_id", unique = true, nullable = false)
-    private Integer id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -24,11 +24,11 @@ public class City {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "city")
     private List<Room> rooms;
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/ua/softserveinc/tc/entity/Room.java
+++ src/main/java/ua/softserveinc/tc/entity/Room.java
@@ -14,7 +14,7 @@ public class Room {
     @GenericGenerator(name = "generator", strategy = "increment")
     @GeneratedValue(generator = "generator")
     @Column(name = "room_id", unique = true, nullable = false)
-    private Integer id;
+    private Long id;
 
     @Column(name = "name")
     private String name;
@@ -33,11 +33,11 @@ public class Room {
     private City city;
 
 
-    public Integer getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
