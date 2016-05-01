375ede8e6359bd890f3b741d9386a5dacf801e3a
Edward Litvinchuck <elitvinchuck@gmail.com>
2016-05-01 11:50:22 +0300

    update RoomDao

--- src/main/java/ua/softserveinc/tc/dao/RoomDao.java
+++ src/main/java/ua/softserveinc/tc/dao/RoomDao.java
@@ -1,7 +1,13 @@
 package ua.softserveinc.tc.dao;
 
+import java.util.List;
+
+import ua.softserveinc.tc.entity.City;
 import ua.softserveinc.tc.entity.Room;
+import ua.softserveinc.tc.entity.User;
 
 public interface RoomDao extends BaseDao<Room> {
+    List<Room> findByCity(City city);
 
+    List<Room> findByManager(User user);
 }
--- src/main/java/ua/softserveinc/tc/dao/RoomDaoImpl.java
+++ src/main/java/ua/softserveinc/tc/dao/RoomDaoImpl.java
@@ -1,9 +1,29 @@
 package ua.softserveinc.tc.dao;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import ua.softserveinc.tc.entity.City;
 import ua.softserveinc.tc.entity.Room;
+import ua.softserveinc.tc.entity.User;
 
-@Repository
+@Repository("roomDao")
 public class RoomDaoImpl extends BaseDaoImpl<Room> implements RoomDao {
+    @Override
+    public List<Room> findByCity(City city) {
+        return getEntityManager()
+                .createQuery("SELECT r FROM Room r WHERE r.city = ?1", Room.class)
+                .setParameter(1, city)
+                .getResultList();
+    }
 
+    @Override
+    public List<Room> findByManager(User user) {
+        return getEntityManager()
+                .createQuery("SELECT r FROM Room r WHERE r.manager = ?1", Room.class)
+                .setParameter(1, user)
+                .getResultList();
+    }
 }
