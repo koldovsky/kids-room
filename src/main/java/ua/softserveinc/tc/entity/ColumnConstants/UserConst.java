9b010fdcef7dcb55f10ffa5d9c25bf8ca29fff26
Petryshakvasyl <vasylpetryshak@gmail.com>
2016-05-05 23:40:05 +0300

    Add UserConstants

--- src/main/java/ua/softserveinc/tc/entity/ColumnConstants/UserConst.java
+++ src/main/java/ua/softserveinc/tc/entity/ColumnConstants/UserConst.java
@@ -4,4 +4,18 @@ package ua.softserveinc.tc.entity.ColumnConstants;
  * Created by Chak on 05.05.2016.
  */
 public interface UserConst {
+
+    String ID_USER = "id_user";
+
+    String FIRST_NAME = "first_name";
+
+    String LAST_NAME = "last_name";
+
+    String EMAIL = "email";
+
+    String PASSWORD = "password";
+
+    String PHONE = "phone-number";
+
+    String ROLE = "role";
 }
