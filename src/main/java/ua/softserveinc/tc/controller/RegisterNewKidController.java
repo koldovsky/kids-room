bae7eeba910a91def07e36f76a96da3e6bd11dcc
nestorsokil <nestor0603@gmail.com>
2016-05-08 23:26:07 +0300

    Kid registration fully implemented.

--- src/main/java/ua/softserveinc/tc/controller/RegisterNewKidController.java
+++ src/main/java/ua/softserveinc/tc/controller/RegisterNewKidController.java
@@ -3,7 +3,9 @@ package ua.softserveinc.tc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import ua.softserveinc.tc.entity.Child;
 import ua.softserveinc.tc.service.ChildService;
 import ua.softserveinc.tc.service.UserService;
@@ -15,26 +17,24 @@ import java.security.Principal;
  */
 
 @Controller
-public final class RegisterNewKidController {
+public class RegisterNewKidController {
     @Autowired
     private ChildService childService;
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/registerkid", method = RequestMethod.GET)
-    public String registerKid(Model model) {
+    @RequestMapping(value = "/registerkid**", method = RequestMethod.GET)
+    public String registerKid(Model model){
         model.addAttribute("child", new Child());
         return "registerkid";
     }
 
-    @RequestMapping(value = "/registerkid", method = RequestMethod.POST)
-    public String submit(
-            @ModelAttribute Child child,
-            Principal principal) {
+    @RequestMapping(value="/registerkid**", method = RequestMethod.POST)
+    public String submit(@ModelAttribute Child child, Principal principal){
         child.setParentId(
-                        userService.getUserByEmail(
-                                        principal.getName()));
+                userService.getUserByEmail(
+                        principal.getName()));
         childService.create(child);
         return "registerkid";
     }
