7698d6de11ba6ee1d083d6e972aa7e706a3bf767
EditedBoy <editedboy19@gmail.com>
2016-05-08 04:09:52 +0300

    update service package. added class for city entity

---
+++ src/main/java/ua/softserveinc/tc/service/CityService.java
@@ -0,0 +1,9 @@
+package ua.softserveinc.tc.service;
+
+import ua.softserveinc.tc.entity.City;
+
+/**
+ * Created by TARAS on 08.05.2016.
+ */
+public interface CityService extends BaseService<City> {
+}
