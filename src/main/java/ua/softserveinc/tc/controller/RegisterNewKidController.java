4058235b658590ecded850fe599c9fe88bf567d6
nestorsokil <nestor0603@gmail.com>
2016-05-08 23:19:11 +0300

    Kid registration fully implemented.

--- src/main/java/ua/softserveinc/tc/controller/RegisterNewKidController.java
+++ src/main/java/ua/softserveinc/tc/controller/RegisterNewKidController.java
@@ -3,9 +3,7 @@ package ua.softserveinc.tc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import ua.softserveinc.tc.entity.Child;
 import ua.softserveinc.tc.service.ChildService;
 import ua.softserveinc.tc.service.UserService;
@@ -17,22 +15,26 @@ import java.security.Principal;
  */
 
 @Controller
-public class RegisterNewKidController {
+public final class RegisterNewKidController {
     @Autowired
     private ChildService childService;
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/registerkid**", method = RequestMethod.GET)
-    public String registerKid(Model model){
+    @RequestMapping(value = "/registerkid", method = RequestMethod.GET)
+    public String registerKid(Model model) {
         model.addAttribute("child", new Child());
         return "registerkid";
     }
 
-    @RequestMapping(value="/registerkid**", method = RequestMethod.POST)
-    public String submit(@ModelAttribute Child child, Principal principal){
-        child.setParentId(userService.getUserByEmail(principal.getName()));
+    @RequestMapping(value = "/registerkid", method = RequestMethod.POST)
+    public String submit(
+            @ModelAttribute Child child,
+            Principal principal) {
+        child.setParentId(
+                        userService.getUserByEmail(
+                                        principal.getName()));
         childService.create(child);
         return "registerkid";
     }
--- src/main/webapp/WEB-INF/views/registerkid.jsp
+++ src/main/webapp/WEB-INF/views/registerkid.jsp
@@ -1,9 +1,11 @@
 <%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
-<div class="col-sm-2">
-    <form:form action="registerkid" method="post" modelAttribute="child">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
+
+<form:form class = "registerkid" enctype="application/x-www-form-urlencoded" action="registerkid" method="post" modelAttribute="child">
+
+      <h2>Child Registration</h2>
       <form:hidden path="id" />
-      <div class="form-group">
 
       <div class="form-group">
              <label for="firstname" class="required">First Name</label>
@@ -14,6 +16,15 @@
               <form:input path="lastName" id="lastname" class="form-control" required="required" />
       </div>
 
+      <div class="form-group">
+                    <label for="date" class="required">Date of birth</label>
+                    <form:input path="dateOfBirth" id="date" name="date" class="form-control" required="required" />
+      </div>
+
+      <div class="form-group">
+                    <label for="comment">Comments</label>
+                    <form:textarea path="comment" id="comment" rows="7" cols="50" class="form-control"/>
+      </div>
+
       <button type="submit" class="btn btn-default">Submit</button>
 </form:form>
-</div>
