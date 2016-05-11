1d707cb2ca15508763cbce114723c9f3748b8586
EditedBoy <editedboy19@gmail.com>
2016-05-12 02:39:03 +0300

    interface AdminConst

---
+++ src/main/java/ua/softserveinc/tc/constants/ModelConstants/AdminConst.java
@@ -0,0 +1,15 @@
+package ua.softserveinc.tc.constants.ModelConstants;
+
+/**
+ * Created by TARAS on 12.05.2016.
+ */
+public interface AdminConst {
+
+    String EDIT_MANAGER_VIEW = "adm-edit-manager";
+
+    String EDIT_LOCATION_VIEW = "adm-edit-location";
+
+    String ADD_LOCATION_VIEW = "adm-add-manager";
+
+    String ADD_MANAGER_VIEW = "adm-add-location";
+}
