b2af4fbea84c2e9e1547a60fdabccb52dc7d3387
Edward Litvinchuck <elitvinchuck@gmail.com>
2016-05-14 22:06:47 +0300

    add Hibernate Search to project

---
+++ src/main/java/ua/softserveinc/tc/search/UserSearch.java
@@ -0,0 +1,56 @@
+package ua.softserveinc.tc.search;
+
+import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
+import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.QueryBuilder;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import ua.softserveinc.tc.entity.User;
+
+/**
+ * Created by edward on 5/14/16.
+ */
+@Repository
+@Transactional
+public class UserSearch {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public List<User> search(String text) {
+        FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
+        try {
+            fullTextEntityManager.createIndexer().startAndWait();
+        } catch (Exception e) {
+
+        }
+
+        QueryBuilder queryBuilder =
+                fullTextEntityManager.getSearchFactory()
+                        .buildQueryBuilder().forEntity(User.class).get();
+
+        org.apache.lucene.search.Query query =
+                queryBuilder
+                        .keyword()
+                        .onFields("firstName", "lastName", "email", "phoneNumber")
+                        .matching(text)
+                        .createQuery();
+
+        javax.persistence.Query persistenceQuery =
+                fullTextEntityManager.createFullTextQuery(query, User.class);
+
+        @SuppressWarnings("unchecked")
+        List<User> results = persistenceQuery.getResultList();
+
+        return results;
+    }
+
+}
