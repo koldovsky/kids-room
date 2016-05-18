f8f4727eaf335ca372a7808ef22fbb4ee74f0035
EditedBoy <editedboy19@gmail.com>
2016-05-19 00:21:25 +0300

    Update admin const's

--- src/main/java/ua/softserveinc/tc/constants/ModelConstants/AdminConst.java
+++ src/main/java/ua/softserveinc/tc/constants/ModelConstants/AdminConst.java
@@ -9,15 +9,21 @@ public interface AdminConst {
 
     String EDIT_MANAGER = "adm-edit-manager";
 
-    String EDIT_LOCATION = "adm-edit-location";
-
-    String ADD_LOCATION = "adm-add-location";
-
     String ADD_MANAGER = "adm-add-manager";
 
     String UPDATE_MANAGER = "adm-update-manager";
 
-    String UPDATE_LOCATION = "adm-update-location";
+
+    String EDIT_ROOM = "adm-edit-room";
+
+    String ADD_ROOM = "adm-add-room";
+
+    String UPDATE_ROOM = "adm-update-room";
+
+
+    String EDIT_CITY = "adm-edit-city";
+
+    String ADD_CITY = "adm-add-city";
 
     ///////////////////////////////////////
 
@@ -30,7 +36,12 @@ public interface AdminConst {
 
     String CITY_LIST = "cityList";
 
+
     String ATR_MANAGER = "manager";
+
+    String ATR_ROOM = "room";
+
+    String ATR_CITY = "city";
 
     ///////////////////////////////////////
 
