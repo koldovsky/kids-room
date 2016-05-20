15c028bd1f404ec35b0afed0ec40e20294c006e8
vrogulya <volodymyr.rogulya@gmail.com>
2016-05-20 23:10:16 +0300

    tokendaoimpl

--- src/main/java/ua/softserveinc/tc/config/SecurityConfig.java
+++ src/main/java/ua/softserveinc/tc/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import ua.softserveinc.tc.constants.ModelConstants.UsersConst;
 
 
 @ComponentScan(basePackages = "ua.softserveinc.tc.service")
@@ -23,7 +24,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    @Qualifier("userDetailsService")
+    @Qualifier(UsersConst.USER_DETAILS_SERVICE)
     private UserDetailsService userDetailsService;
 
     @Autowired
---
+++ src/main/java/ua/softserveinc/tc/dao/TokenDaoImpl.java
@@ -0,0 +1,36 @@
+package ua.softserveinc.tc.dao;
+
+import org.springframework.stereotype.Repository;
+import ua.softserveinc.tc.constants.ModelConstants.TokenConst;
+import ua.softserveinc.tc.constants.ModelConstants.UsersConst;
+import ua.softserveinc.tc.entity.Token;
+import ua.softserveinc.tc.entity.User;
+
+import javax.persistence.Query;
+
+
+/**
+ * Created by Chak on 17.05.2016.
+ */
+@Repository
+public class TokenDaoImpl extends BaseDaoImpl<Token> implements TokenDao {
+
+    @Override
+    public void delete(Token entity) {
+        super.delete(getEntityManager().merge(entity));
+    }
+
+    @Override
+    public Token findByUser(User user) {
+        Query query = getEntityManager().createQuery("from Token where user.id = :user");
+        query.setParameter(UsersConst.USER, user.getId());
+        return (Token) query.getSingleResult();
+    }
+
+    @Override
+    public Token findByToken(String token) {
+        Query query = getEntityManager().createQuery("from Token where token = :token");
+        query.setParameter(TokenConst.TOKEN, token);
+        return (Token) query.getSingleResult();
+    }
+}
