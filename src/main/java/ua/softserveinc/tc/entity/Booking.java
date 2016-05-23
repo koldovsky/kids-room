2962df082aad9cb4f21eb08b2401218326cc6b84
Demian <bahrianyi@ukr.net>
2016-05-23 19:01:16 +0300

    fixed bug in report.js

--- src/main/java/ua/softserveinc/tc/entity/Booking.java
+++ src/main/java/ua/softserveinc/tc/entity/Booking.java
@@ -34,7 +34,6 @@ public class Booking {
     @ManyToOne(optional = false)
     @JoinColumn(name = UserConst.ID_USER)
     private User idUser;
-    //а чо в мене комітиться?
 
     @Column(name = BookingConst.BOOKING_START_TIME, nullable = false)
     private Date bookingStartTime;
