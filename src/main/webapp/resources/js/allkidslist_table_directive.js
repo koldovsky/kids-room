4c0b133c32947772ff247eb27d84584c9e74a121
Edward Litvinchuck <elitvinchuck@gmail.com>
2016-05-24 11:08:31 +0300

    Divide angular app

---
+++ src/main/webapp/resources/js/allkidslist_table_directive.js
@@ -0,0 +1,22 @@
+
+'use strict';
+
+
+function AllKidsTable() {
+
+    var link = function (scope, element, attrs) {
+    }
+
+    var compile = function() {
+
+    }
+
+    return {
+        restrict: 'E',
+        templateUrl: 'resources/templates/allkidstable.html',
+        link: link,
+        controller: 'AllKidsTableController',
+        scope: {
+        }
+    };
+}
---
+++ src/main/webapp/resources/js/allkidslist_table_service.js
@@ -0,0 +1,67 @@
+
+'use strict';
+
+function AllKidsTableService($http, $q) {
+
+    return({
+        getChildren: getChildren,
+        searchChildren: searchChildren,
+        getParent: getParent
+    });
+
+    function getChildren() {
+
+        var request = $http({
+            method: "get",
+            url: "api/child",
+            params: {
+                action: "get"
+            }
+        });
+
+        return ( request.then( handleSuccess, handleError ) );
+    }
+
+    function searchChildren(field) {
+
+        var request = $http({
+            method: "get",
+            url: "api/child/search",
+            params: {
+                action: "get",
+                field: field
+            }
+        });
+
+        return ( request.then( handleSuccess, handleError ) );
+    }
+
+    function getParent( childId ) {
+
+        var request = $http({
+            method: "get",
+            url: "api/child/" + childId + "/parent",
+            params: {
+                action: "get"
+            }
+        });
+
+        return ( request.then( handleSuccess, handleError ) );
+    }
+
+    function handleError( response ) {
+
+        if (
+            ! angular.isObject( response.data ) ||
+            ! response.data.message
+            ) {
+            return( $q.reject( "An unknown error occurred." ) );
+        }
+
+        return( $q.reject( response.data.message ) );
+    }
+
+    function handleSuccess( response ) {
+        return( response.data );
+    }
+}
