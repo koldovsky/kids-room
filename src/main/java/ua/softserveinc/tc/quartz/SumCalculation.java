ecfd77b4ac63dc095014e9fcde451d23d4f56cef
Demian <bahrianyi@ukr.net>
2016-05-28 02:06:37 +0300

    added support of quartz job

---
+++ src/main/java/ua/softserveinc/tc/quartz/SumCalculation.java
@@ -0,0 +1,24 @@
+package ua.softserveinc.tc.quartz;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import ua.softserveinc.tc.entity.Booking;
+import ua.softserveinc.tc.service.BookingService;
+
+import java.util.List;
+
+/**
+ * Created by Demian on 28.05.2016.
+ */
+public class SumCalculation implements Job
+{
+    @Autowired
+    BookingService bookingService;
+
+    public void execute(JobExecutionContext context)
+    {
+        List<Booking> bookings = bookingService.getBookingsWithZeroSum();
+        bookings.forEach(booking -> bookingService.calculateSum(booking));
+    }
+}
