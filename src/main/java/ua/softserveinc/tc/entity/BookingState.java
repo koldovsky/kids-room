e2637d65367abfa72a5a7ad10b8e4d53b2132f9f
Demian <bahrianyi@ukr.net>
2016-06-10 21:26:08 +0300

    Lv-181.Java | LVJAVS-122 | Added new booking state

--- src/main/java/ua/softserveinc/tc/entity/BookingState.java
+++ src/main/java/ua/softserveinc/tc/entity/BookingState.java
@@ -4,9 +4,9 @@ package ua.softserveinc.tc.entity;
  * Created by Петришак on 28.05.2016.
  */
 public enum BookingState {
-
     BOOKED,
     ACTIVE,
+    CALCULATE_SUM,
     COMPLETED,
-    CANCELLED;
+    CANCELLED
 }
