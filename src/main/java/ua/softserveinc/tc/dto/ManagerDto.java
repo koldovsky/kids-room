f7a5860f737a36e5592d6a3e1937806bd9bd402e
EditedBoy <editedboy19@gmail.com>
2016-06-13 02:35:29 +0300

    Lv-181.Java | LVJAVS-130 | Add & Edit room Validator | dto part

---
+++ src/main/java/ua/softserveinc/tc/dto/ManagerDto.java
@@ -0,0 +1,18 @@
+package ua.softserveinc.tc.dto;
+
+
+/**
+ * Created by TARAS on 05.06.2016.
+ */
+public class ManagerDto {
+
+    private String managerId;
+
+    public String getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(String managerId) {
+        this.managerId = managerId;
+    }
+}
