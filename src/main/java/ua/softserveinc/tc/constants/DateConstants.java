abcddf08a07d1480556d474099174c5322709bdc
vrogulya <volodymyr.rogulya@gmail.com>
2016-06-15 00:21:50 +0300

    Lv-181.Java | LVJAVS-122 | code cleaner and cleaner (19/20 completed)

--- src/main/java/ua/softserveinc/tc/constants/DateConstants.java
+++ src/main/java/ua/softserveinc/tc/constants/DateConstants.java
@@ -5,8 +5,11 @@ package ua.softserveinc.tc.constants;
  */
 public final class DateConstants {
     public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
     public static final String SHORT_DATE_FORMAT = "yyyy-MM-dd";
+
     public static final String DATE_AND_TIME_FORMAT = "yyyy-MM-dd HH:mm";
+
     public static final String TIME_FORMAT = "HH:mm";
 
     private DateConstants() {
