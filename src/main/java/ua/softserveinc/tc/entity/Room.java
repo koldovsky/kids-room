5b9c980895b264d3f563af99a4338d501aa03e6b
vrogulya <volodymyr.rogulya@gmail.com>
2016-06-23 11:32:41 +0300

    Lv-181.Java | LVJAVS-122 | fix issues

--- src/main/java/ua/softserveinc/tc/entity/Room.java
+++ src/main/java/ua/softserveinc/tc/entity/Room.java
@@ -181,8 +181,12 @@ public class Room {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Room room = (Room) o;
 
