873e8530ec5343a12db3466b32a7c7856e0e6e52
EditedBoy <editedboy19@gmail.com>
2016-06-28 14:45:19 +0300

    Lv-181.Java | LVJAVS-122 | Bug fix

--- src/main/java/ua/softserveinc/tc/config/SecurityConfig.java
+++ src/main/java/ua/softserveinc/tc/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("resources/css/**").permitAll()
                 .antMatchers("resources/js/**").permitAll()
