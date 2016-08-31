b61ff23b0953fe0835d96c3dce69cce84af47477
Kononchuk-B <danon.konon@gmail.com>
2016-08-31 19:21:17 +0300

    Now there is no possibility to choose the date in single booking

---
+++ src/main/webapp/resources/js/single-booking.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.choose-booking').click(function() {
+        if ($("input[id='weekly-booking']:checked").val()) {
+            $('#recurrent-booking-end-date').prop("disabled", false)
+        } else {
+            $('#recurrent-booking-start-date').change(function() {
+                $('#recurrent-booking-end-date').val($(this).val())
+            });
+            $('#recurrent-booking-end-date').prop("disabled", true)
+        }
+    });
+});
