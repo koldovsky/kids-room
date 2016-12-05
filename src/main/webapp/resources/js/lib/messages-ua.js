4af6944da31df1e193aebecffb8cff9878a7e7a5
VitalijF <vitalijfedyna@gmail.com>
2016-12-06 00:18:36 +0200

    Convention on quotes

--- src/main/webapp/resources/js/lib/messages-ua.js
+++ src/main/webapp/resources/js/lib/messages-ua.js
@@ -3,7 +3,7 @@
  */
 messages = {
 	kid : {
-		invalidFirstName : "Ви ввели ім'я невірно",
+		invalidFirstName : 'Ви ввели ім\'я невірно',
 		invalidLastName : 'Ви ввели прізвище невірно',
 		requiredFirstName: 'Вкажіть будь ласка ім\'я дитини',
 		requiredLastName: 'Вкажіть будь ласка прізвище дитини',
@@ -15,54 +15,54 @@ messages = {
         toLongLastName : 'Будь ласка введіть менше ' + nameMaxLength + ' символів.'
 	},
     date: {
-        fromBiggerThanTo:  "Початкова дата не повинна перевищувати кінцеву",
-        startEventDate: "Початок: ",
-        endEventDate: "Кінець: "
+        fromBiggerThanTo:  'Початкова дата не повинна перевищувати кінцеву',
+        startEventDate: 'Початок: ',
+        endEventDate: 'Кінець: '
     },
     info: {
-        description: "Опис: ",
-        noDescription: "Ця подія немає опису",
-        fromBiggerThanTo:  "Початкова дата не повинна перевищувати кінцеву"
+        description: 'Опис: ',
+        noDescription: 'Ця подія немає опису',
+        fromBiggerThanTo:  'Початкова дата не повинна перевищувати кінцеву'
     },
 	dateTable: {
-		emptyTable: "Немає даних в таблиці",
-		processing: "Зачекайте...",
-		search: "Пошук:",
-		lengthMenu: "Показати _MENU_ записів",
-		info: "Записи з _START_ по _END_ із _TOTAL_ записів",
-		infoEmpty: "Записи з 0 по 0 із 0 записів",
-		infoFiltered: "(відфільтровано з _MAX_ записів)",
-		loadingRecords: "Завантаження...",
-		zeroRecords: "Записи відсутні",
+		emptyTable: 'Немає даних в таблиці',
+		processing: 'Зачекайте...',
+		search: 'Пошук:',
+		lengthMenu: 'Показати _MENU_ записів',
+		info: 'Записи з _START_ по _END_ із _TOTAL_ записів',
+		infoEmpty: 'Записи з 0 по 0 із 0 записів',
+		infoFiltered: '(відфільтровано з _MAX_ записів)',
+		loadingRecords: 'Завантаження...',
+		zeroRecords: 'Записи відсутні',
 		paginate: {
-			first: "Перша",
-			previous: "Попередня",
-			next: "Наступна",
-			last: "Остання"
+			first: 'Перша',
+			previous: 'Попередня',
+			next: 'Наступна',
+			last: 'Остання'
 		},
         aria: {
-			sortAscending:  ": активувати для сортування стовпців за зростанням",
-			sortDescending: ": активувати для сортування стовпців за спаданням"
+			sortAscending:  ': активувати для сортування стовпців за зростанням',
+			sortDescending: ': активувати для сортування стовпців за спаданням'
 		}
 	},
 	modal:
 	{
 		kid: {
-			comment: "Коментарій",
-			choose: "Будь ласка виберіть дитину"
+			comment: 'Коментарій',
+			choose: 'Будь ласка виберіть дитину'
 		}
 	},
 	event: {
 		errors: {
-			dateInThePast: "Дата не може бути в минулому, поточна дата: ",
-			timeInThePast: "Час початку не може бути в минулому, поточний час: ",
-			endTimeGreaterThanStartTime: "Час закінчення повинен бути хоча б на хвилину більший за час початку",
-			minimalDatesDifference: "Повторювані: Дата закінчення повинна бути хоча б на день більшою за дату початку",
-			emptyTitle: "Назва повинна бути заповнена",
-			noDaysSelected: "Повторювані: Як мінімум один день повинен бути вибраним",
-			noKidsSelected: "Як мінімум одна дитина повинна бути вибрана",
-			bookingTypeMismatchWhenUpdating: "Неможливо перетворити щотижневе бронювання в одноденне",
-			incorrectData: "Невірні дані: "
+			dateInThePast: 'Дата не може бути в минулому, поточна дата: ',
+			timeInThePast: 'Час початку не може бути в минулому, поточний час: ',
+			endTimeGreaterThanStartTime: 'Час закінчення повинен бути хоча б на хвилину більший за час початку',
+			minimalDatesDifference: 'Повторювані: Дата закінчення повинна бути хоча б на день більшою за дату початку',
+			emptyTitle: 'Назва повинна бути заповнена',
+			noDaysSelected: 'Повторювані: Як мінімум один день повинен бути вибраним',
+			noKidsSelected: 'Як мінімум одна дитина повинна бути вибрана',
+			bookingTypeMismatchWhenUpdating: 'Неможливо перетворити щотижневе бронювання в одноденне',
+			incorrectData: 'Невірні дані: '
 		}
     },
 	notCorrect: {
@@ -70,10 +70,10 @@ messages = {
 		startDate: 'Введено неправильну початкову дату. Допустимий формат: <b>dd:MM:YYYY</b>',
 		endDate: 'Введено неправильну кінцеву дату. Допустимий формат: <b>dd:MM:YYYY</b>',
 		server: '<b>невідомо</b>. Помилка сервера',
-		emptyStartDate: "Не введено початкову дату",
-		emptyEndDate: "Не введено кінцеву дату",
-		pastStartDay: "Початкова дата на може бути в минулому",
-		pastEndDay: "Кінцева дата не може бути меншою за початкову",
+		emptyStartDate: 'Не введено початкову дату',
+		emptyEndDate: 'Не введено кінцеву дату',
+		pastStartDay: 'Початкова дата на може бути в минулому',
+		pastEndDay: 'Кінцева дата не може бути меншою за початкову',
 		wrongDateStartRange: 'Початкова дата є поза межами вибраних заходів',
 		wrongDateEndRange: 'Кінцева дата є поза межами вибраних заходів'
 	}
