821bdbd926ece75810dc5d28e1f1ac6c2c7c3134
VitalijF <vitalijfedyna@gmail.com>
2016-12-24 22:53:08 +0200

    Use correct Name regexp

--- src/main/webapp/resources/js/validation/validation-room.js
+++ src/main/webapp/resources/js/validation/validation-room.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
                     minlength: 1,
                     maxlength: 255,
                     requiredWithEmptySpace: true,
-                    regexName: /^[a-zA-Zа-щА-ЩЬьЮюЯяЇїІіЄєҐґ0-9\s]+$/
+                    regexName: nameRegex
                 },
                 address: {
                     minlength: 1,
@@ -32,13 +32,13 @@ $(document).ready(function () {
                     minlength: 1,
                     maxlength: 255,
                     requiredWithEmptySpace: true,
-                    regexCity: /^[a-zA-Zа-щА-ЩЬьЮюЯяЇїІіЄєҐґ]+$/
+                    regexCity: nameRegex
                 },
                 phoneNumber: {
                     minlength: 1,
                     maxlength: 255,
                     requiredWithEmptySpace: true,
-                    regexPhone: /^\+(?:[0-9] ?){6,14}[0-9]$/
+                    regexPhone: phoneRegex
                 },
                 capacity: {
                     min: 1,
