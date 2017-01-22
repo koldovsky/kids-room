31b2cf9eec34765a248d69c7b6b7c2145da64ef7
Andrii Okhrymovych <andryi2010@ukr.net>
2017-01-22 19:56:58 +0200

    deleted line

--- src/main/java/ua/softserveinc/tc/controller/manager/BookingEditController.java
+++ src/main/java/ua/softserveinc/tc/controller/manager/BookingEditController.java
@@ -156,7 +156,6 @@ public class BookingEditController {
             booking.setComment(bookingDto.getComment());
             booking.setRecurrentId(null);
             bookingService.update(booking);
-
         } else {
             isPossible = false;
         }
