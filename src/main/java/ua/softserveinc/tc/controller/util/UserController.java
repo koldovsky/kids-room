920d749bb4c81e2ce01836afcfe2f83110592acd
Andrii Okhrymovych <andryi2010@ukr.net>
2017-01-27 17:15:36 +0200

    delete unnecessary modal passing

--- src/main/java/ua/softserveinc/tc/controller/util/UserController.java
+++ src/main/java/ua/softserveinc/tc/controller/util/UserController.java
@@ -1,14 +1,9 @@
 package ua.softserveinc.tc.controller.util;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
-import ua.softserveinc.tc.constants.EntryConstants;
 import ua.softserveinc.tc.constants.ErrorConstants;
 import ua.softserveinc.tc.constants.UserConstants;
 
@@ -16,13 +11,9 @@ import ua.softserveinc.tc.constants.UserConstants;
 @Controller
 public class UserController {
 
-    @Autowired
-    private Environment environment;
-
     @Secured({"ROLE_ANONYMOUS"})
     @GetMapping("/login")
     public String login(Model model) {
-        model.addAttribute(EntryConstants.LOGIN_PROP, environment.getProperty("login.without.sso"));
         return UserConstants.Model.LOGIN_VIEW;
     }
 
@@ -33,6 +24,6 @@ public class UserController {
 
     @GetMapping("/accessDenied")
     public String handleError403() {
-        return  ErrorConstants.ACCESS_DENIED_VIEW;
+        return ErrorConstants.ACCESS_DENIED_VIEW;
     }
 }
