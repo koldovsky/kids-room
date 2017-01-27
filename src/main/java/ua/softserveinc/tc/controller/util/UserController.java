a7478a1e76ecc33397be2f596b70df4e7b81bd7f
Andrii Okhrymovych <andryi2010@ukr.net>
2017-01-27 17:20:04 +0200

    delete unusefull parameter

--- src/main/java/ua/softserveinc/tc/controller/util/UserController.java
+++ src/main/java/ua/softserveinc/tc/controller/util/UserController.java
@@ -2,7 +2,6 @@ package ua.softserveinc.tc.controller.util;
 
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import ua.softserveinc.tc.constants.ErrorConstants;
 import ua.softserveinc.tc.constants.UserConstants;
@@ -13,7 +12,7 @@ public class UserController {
 
     @Secured({"ROLE_ANONYMOUS"})
     @GetMapping("/login")
-    public String login(Model model) {
+    public String login() {
         return UserConstants.Model.LOGIN_VIEW;
     }
 
