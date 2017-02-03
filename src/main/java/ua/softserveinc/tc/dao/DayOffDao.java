ae04cf63a09a8d26f2232ceb2d5789096c6fe5cc
ValeryGutor <leragutor@i.ua>
2017-02-03 13:28:15 +0200

    Created dayOffDao

---
+++ src/main/java/ua/softserveinc/tc/dao/DayOffDao.java
@@ -0,0 +1,7 @@
+package ua.softserveinc.tc.dao;
+
+/**
+ * Created by comp on 03.02.2017.
+ */
+public interface DayOff {
+}
---
+++ src/main/java/ua/softserveinc/tc/dao/impl/DayOffDaoImpl.java
@@ -0,0 +1,7 @@
+package ua.softserveinc.tc.dao.impl;
+
+/**
+ * Created by comp on 03.02.2017.
+ */
+public class DayOffDaoImpl {
+}
