791b0bd89012131efe8c25de7bfd44bd143b49c5
ValeryGutor <leragutor@i.ua>
2017-02-06 10:51:06 +0200

    Add constants to ChildDaoIT.java

--- src/test/java/ua/softserveinc/tc/dao/integration/ChildDaoIT.java
+++ src/test/java/ua/softserveinc/tc/dao/integration/ChildDaoIT.java
@@ -22,6 +22,8 @@ import ua.softserveinc.tc.dao.ChildDao;
 import ua.softserveinc.tc.dao.RoomDao;
 import ua.softserveinc.tc.messaging.ChildMessages;
 
+import static ua.softserveinc.tc.dao.paths.ChildDaoITPath.*;
+
 @Category(IntegrationTest.class)
 @DirtiesContext
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -38,32 +40,32 @@ public class ChildDaoIT {
     @Autowired
     private RoomDao roomDao;
 
-    @DatabaseSetup(value = "classpath:childDao/one-no-active-booking.xml", type = DatabaseOperation.CLEAN_INSERT)
-    @DatabaseTearDown(value = "classpath:childDao/one-no-active-booking.xml", type = DatabaseOperation.DELETE_ALL)
+    @DatabaseSetup(value = NO_ACTIVE_BOOKINGS, type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseTearDown(value = NO_ACTIVE_BOOKINGS, type = DatabaseOperation.DELETE_ALL)
     @Test
     public void testGetActiveChildrenInRoomWhenRoomIsNotActive() {
         Assert.assertEquals(ChildMessages.INCORRECT_GET_ACTIVE_CHILDREN_IN_ROOM_WHEN_ROOM_IS_NOT_ACTIVE,
                 0, childDao.getActiveChildrenInRoom(roomDao.findById(1L)).size());
     }
 
-    @DatabaseSetup(value = "classpath:childDao/one-active-booking.xml", type = DatabaseOperation.CLEAN_INSERT)
-    @DatabaseTearDown(value = "classpath:childDao/one-active-booking.xml", type = DatabaseOperation.DELETE_ALL)
+    @DatabaseSetup(value = ONE_ACTIVE_BOOKING, type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseTearDown(value = ONE_ACTIVE_BOOKING, type = DatabaseOperation.DELETE_ALL)
     @Test
     public void testGetActiveChildrenInRoomWhenRoomIsActive() {
         Assert.assertEquals(ChildMessages.INCORRECT_GET_ACTIVE_CHILDREN_IN_ROOM_WHEN_ROOM_IS_ACTIVE,
                 1, childDao.getActiveChildrenInRoom(roomDao.findById(1L)).size());
     }
 
-    @DatabaseSetup(value = "classpath:childDao/multiple-active-booking.xml", type = DatabaseOperation.CLEAN_INSERT)
-    @DatabaseTearDown(value = "classpath:childDao/multiple-active-booking.xml", type = DatabaseOperation.DELETE_ALL)
+    @DatabaseSetup(value = MULTIPLE_ACTIVE_BOOKINGS, type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseTearDown(value = MULTIPLE_ACTIVE_BOOKINGS, type = DatabaseOperation.DELETE_ALL)
     @Test
     public void testGetActiveChildrenInRoomWhenRoomAreActive() {
         Assert.assertEquals(ChildMessages.INCORRECT_GET_ACTIVE_CHILDREN_IN_ROOM_WHEN_ROOM_IS_ACTIVE,
                 2, childDao.getActiveChildrenInRoom(roomDao.findById(1L)).size());
     }
 
-    @DatabaseSetup(value = "classpath:childDao/multiple-no-active-booking.xml", type = DatabaseOperation.CLEAN_INSERT)
-    @DatabaseTearDown(value = "classpath:childDao/multiple-no-active-booking.xml", type = DatabaseOperation.DELETE_ALL)
+    @DatabaseSetup(value = MULTIPLE_NO_ACTIVE_BOOKINGS, type = DatabaseOperation.CLEAN_INSERT)
+    @DatabaseTearDown(value = MULTIPLE_NO_ACTIVE_BOOKINGS, type = DatabaseOperation.DELETE_ALL)
     @Test
     public void testGetActiveChildrenInRoomWhenRoomAreNoActive() {
         Assert.assertEquals(ChildMessages.INCORRECT_GET_ACTIVE_CHILDREN_IN_ROOM_WHEN_ROOM_IS_ACTIVE,
---
+++ src/test/java/ua/softserveinc/tc/dao/paths/ChildDaoITPath.java
@@ -0,0 +1,13 @@
+package ua.softserveinc.tc.dao.paths;
+
+public final class ChildDaoITPath {
+
+    public static final String NO_ACTIVE_BOOKINGS = "classpath:childDao/one-no-active-booking.xml";
+
+    public static final String ONE_ACTIVE_BOOKING = "classpath:childDao/one-active-booking.xml";
+
+    public static final String MULTIPLE_ACTIVE_BOOKINGS = "classpath:childDao/multiple-active-booking.xml";
+
+    public static final String MULTIPLE_NO_ACTIVE_BOOKINGS = "classpath:childDao/multiple-no-active-booking.xml";
+
+}
