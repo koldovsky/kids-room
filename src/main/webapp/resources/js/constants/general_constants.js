4ef302b097eb6215a83904ca3509850a935ef23f
Savshak <savshakbohdan@gmail.com>
2017-03-10 13:24:19 +0200

    fixed - user is able to type any text or symbols of any length in time field while creating new event

--- src/main/webapp/resources/js/constants/general_constants.js
+++ src/main/webapp/resources/js/constants/general_constants.js
@@ -4,7 +4,8 @@ const constants = {
         phoneRegex: /^\+(?:[0-9] ?){6,14}[0-9]$/,
         emailRegex: /^[_a-zA-Z0-9-]+(\.[_a-zA-Z0-9-]+)*@(([0-9]{1,3})|([a-zA-Z]{2,11})|(aero|coop|info|museum|name))+(\\.(([0-9]{1,3})|([a-zA-Z]{2,3})|(aero|coop|info|museum|name)))*\.(([0-9]{1,3})|([a-zA-Z]{2,3})|(aero|coop|info|museum|name))*$/,
         timeRegex: /\d{2}:\d{2}/,
-        addresssRegex: /^[a-zA-ZАа-щА-ЩЬьЮюЯяЇїІіЄєҐґ0-9\s]+$/
+        addresssRegex: /^[a-zA-ZАа-щА-ЩЬьЮюЯяЇїІіЄєҐґ0-9\s]+$/,
+        twoNumberRegex: /([0-9]{2})([0-9]{2})/i
     },
     room: {
         warnings: {
--- src/main/webapp/resources/js/header-manager.js
+++ src/main/webapp/resources/js/header-manager.js
@@ -4,6 +4,25 @@ $(function () {
         setDate: moment().format(constants.parameters.dateFormatUpperCase)
     });
 
+    $('.picker').keypress(function(e) {
+        if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+            return false;
+        } else {
+            function format(e) {
+                var r = constants.regex.twoNumberRegex,
+                    str = e.target.value.replace(/[^0-9]/ig, "");
+
+                while(r.test(str)) {
+                    str = str.replace(r, '$1' + ':' + '$2');
+                }
+
+                e.target.value = str.slice(0,5);
+            }
+
+            $('.picker').keyup(format);
+        }
+    });
+
     var currentDate = moment().format(constants.parameters.dateFormatUpperCase);
     var listRoom = $('#selectRoom li a');
     var inactiveRoom = true;
