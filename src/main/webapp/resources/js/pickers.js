9f8832e793dff654b7c62bfb7ca3305a71f341ff
seltsamD <nahod.dar@gmail.com>
2017-03-30 22:05:55 +0300

    FIX-LVJAVS-867: Add validation for datepicker.

--- src/main/webapp/resources/js/pickers.js
+++ src/main/webapp/resources/js/pickers.js
@@ -2,7 +2,23 @@ $('.datepickers').datepicker({
     dateFormat: constants.parameters.dateFormat,
     setDate: moment().format(constants.parameters.dateFormatUpperCase)
 });
+$('.datepickers').keyup(function () {
+    var format = function () {
+        if(moment(this.value, constants.parameters.dateFormatUpperCase, true).isValid()){
+            $(this).removeClass("error");
+        }
+        else {
+            $(this).addClass("error");
+        }
+    };
 
+    $('.datepickers').keyup(format);
+    $('.datepickers').change(format);
+});
+$('.picker').timepicker({
+    timeFormat: 'H:i',
+    step: 1
+});
 $('.picker').attr("maxlength", "5");
 $('.picker').keypress(function(e) {
     if (e.which != constants.keyCodes.backspace && (e.which < constants.keyCodes.zero ||
