104c152439db7c97c273ea60290a7e2b512d039c
Shabala Yevhen <Genya@MacBookPro-Shabala-2.local>
2017-04-05 01:38:35 +0300

    resolve comments

--- src/main/java/ua/softserveinc/tc/util/PaginationCharacteristics.java
+++ src/main/java/ua/softserveinc/tc/util/PaginationCharacteristics.java
@@ -10,16 +10,6 @@ public final class PaginationCharacteristics {
     public static long searchCount = 0;
 
     public static long definePage(long start, long paginationLength, long itemsLength) {
-//        long pagesCount = (long) Math.ceil((double) itemsLength / paginationLength);
-        long ret = start == 0 ? 0 : (long) Math.ceil((double)(start + 1)/paginationLength);
-        System.out.println(ret);
-        return ret;
-//        return (long) Math.ceil((double)(start + 1)/paginationLength);
+        return start == 0 ? 0 : (long) Math.ceil((double)(start + 1)/paginationLength);
     }
-
-//    public static boolean isSearched(List<Search> searches) {
-//        return !searches.parallelStream()
-//                .map(Search::getColumn)
-//                .allMatch(StringUtils::isEmpty);
-//    }
 }
