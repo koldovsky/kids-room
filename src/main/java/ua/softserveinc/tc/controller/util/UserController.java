63a5124e782f06b12ebbb8994faac1085c929437
Tat0 <pro100fartovuy@gmail.com>
2017-04-10 11:28:17 +0300

    Fixed bug when user can click 'back' button after login and see error message

--- src/main/java/ua/softserveinc/tc/controller/util/UserController.java
+++ src/main/java/ua/softserveinc/tc/controller/util/UserController.java
@@ -1,19 +1,34 @@
 package ua.softserveinc.tc.controller.util;
 
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ua.softserveinc.tc.constants.AdminConstants;
+import ua.softserveinc.tc.constants.ChildConstants;
 import ua.softserveinc.tc.constants.ErrorConstants;
+import ua.softserveinc.tc.constants.ManagerConstants;
 import ua.softserveinc.tc.constants.UserConstants;
+import ua.softserveinc.tc.entity.User;
+import ua.softserveinc.tc.service.UserService;
 
 
 @Controller
 public class UserController {
 
-    @Secured({"ROLE_ANONYMOUS"})
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/login")
-    public String login() {
-        return UserConstants.Model.LOGIN_VIEW;
+    public String login(Principal principal) {
+        String resultView;
+        if(principal == null){
+            resultView = UserConstants.Model.LOGIN_VIEW;
+            return resultView;
+        }else{
+           return abort_login_page(principal);
+        }
     }
 
     @GetMapping("/rules")
@@ -24,5 +39,28 @@ public class UserController {
     @GetMapping("/accessDenied")
     public String handleError403() {
         return ErrorConstants.ACCESS_DENIED_VIEW;
+    }
+
+    private String abort_login_page(Principal principal){
+        User user = userService.getUserByEmail(principal.getName());
+        String view;
+        switch (user.getRole()) {
+            case USER:
+                if (user.getChildren().isEmpty() || userService.getEnabledChildren(user).isEmpty()) {
+                    view = "redirect:/" + ChildConstants.View.MY_KIDS;
+                } else {
+                    view = "redirect:/" + UserConstants.USER_CALENDAR;
+                }
+                break;
+            case MANAGER:
+                view = "redirect:/" + ManagerConstants.MANAGER_EDIT_BOOKING;
+                break;
+            case ADMINISTRATOR:
+                view =  "redirect:/" + AdminConstants.EDIT_MANAGER;
+                break;
+            default:
+                view = "redirect:/";
+        }
+        return view;
     }
 }
